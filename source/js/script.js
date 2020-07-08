3f58c0f73d7605f5823ad87bd3736efaa95bd531
rabbit <barateligeorge@yandex.ru>
2020-07-08 15:53:44 +0300

    исправил часть багов

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 let menu = document.querySelector('.navigation');
 let menuToggle = document.querySelector('.navigation__button-toggle');
+let telInput = document.querySelector('.booking__input-field[type="tel"]');
 
 menu.classList.remove('navigation--open');
 
@@ -11,4 +12,8 @@ menuToggle.addEventListener('click', function () {
     menu.classList.add('navigation--open');
     menuToggle.style.display = "inline-block";
   }
+});
+
+telInput.addEventListener('keyup', function(){
+  telInput.value = telInput.value.replace(/[^\d]/g, '');
 });
