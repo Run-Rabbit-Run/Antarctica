26ab626cbb7cefaf977855b5bcb54c773b1b7c11
rabbit <barateligeorge@yandex.ru>
2020-07-09 19:07:55 +0300

    Исправил почти все косяки

--- build/js/script.js
+++ build/js/script.js
@@ -1,19 +1,22 @@
 'use strict';
-let menu = document.querySelector('.navigation');
-let menuToggle = document.querySelector('.navigation__button-toggle');
-let telInput = document.querySelector('.booking__input-field[type="tel"]');
 
-menu.classList.remove('navigation--open');
+(function () {
+  let menu = document.querySelector('.navigation');
+  let menuToggle = document.querySelector('.navigation__button-toggle');
+  let telInput = document.querySelector('.booking__input-field[type="tel"]');
 
-menuToggle.addEventListener('click', function () {
-  if (menu.classList.contains('navigation--open')) {
-    menu.classList.remove('navigation--open');
-  } else {
-    menu.classList.add('navigation--open');
-    menuToggle.style.display = "inline-block";
-  }
-});
+  menu.classList.remove('navigation--open');
 
-telInput.addEventListener('keyup', function(){
-  telInput.value = telInput.value.replace(/[^\d]/g, '');
-});
+  menuToggle.addEventListener('click', function () {
+    if (menu.classList.contains('navigation--open')) {
+      menu.classList.remove('navigation--open');
+    } else {
+      menu.classList.add('navigation--open');
+      menuToggle.style.display = "inline-block";
+    }
+  });
+
+  telInput.addEventListener('keyup', function(){
+    telInput.value = telInput.value.replace(/[^\d]/g, '');
+  });
+})();
